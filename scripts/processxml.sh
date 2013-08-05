d7a825059e5c7e53be76620e9d4cc8bb07b62ab1
Sarah Weissman <seweissman@gmail.com>
2013-08-05 19:22:02 -0400

    Updated xml parse script

--- scripts/processxml.sh
+++ scripts/processxml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #By default, the prefix of a line up to the first tab character is the key and the rest of the line (excluding the tab character) will be the value.
-hadoop jar hadoop-streaming.jar -inputreader "StreamXmlRecord,begin=revision,end=revision" -input $1 -output $2 -mapper -reducer
+hadoop jar hadoop-streaming.jar -inputreader "StreamXmlRecord,begin=revision,end=revision" -input $1 -output $2 -mapper /bin/cat
